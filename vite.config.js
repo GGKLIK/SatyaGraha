e388609ce3d9db1c2f7ff656954209a782a94a24
GGKLIK <Immanuelch55@gmail.com>
2026-07-23 10:37:44 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // Konfigurasi dasar Vite untuk proyek React + Tailwind
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
+  base: '/SatyaGraha/',
 });
