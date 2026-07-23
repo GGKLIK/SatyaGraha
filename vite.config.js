01f90e918f3f0a504bfef223208d41076b5a937c
GGKLIK <Immanuelch55@gmail.com>
2026-07-23 10:57:25 +0700

    Add server settings to Vite configuration
    Added server configuration to Vite for development.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Konfigurasi dasar Vite untuk proyek React + Tailwind
 export default defineConfig({
   plugins: [react()],
-  base: '/SatyaGraha/',
+  base: "/SatyaGraha/",
+  server: {
+    port: 5173,
+    open: true,
+  },
 });
